54cd2c69fcc77bd48df48193288968adb5a1e876
morek <morekszymon@wp.pl>
2020-04-15 19:09:04 +0200

    Adding dialogs for picking date and time

--- app/src/main/java/pl/moras/todoapplication/NewToDoFragment.java
+++ app/src/main/java/pl/moras/todoapplication/NewToDoFragment.java
@@ -2,11 +2,13 @@ package pl.moras.todoapplication;
 
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.DialogFragment;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.annotation.TargetApi;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,9 +17,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 
 import android.widget.CalendarView;
-import android.widget.EditText;
+import android.widget.DatePicker;
 import android.widget.TimePicker;
 import android.widget.Toast;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.textfield.TextInputEditText;
 
 import pl.moras.model.ToDoEvent;
 
@@ -27,72 +32,64 @@ import pl.moras.viewmodel.ToDoViewModel;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalField;
 import java.util.Calendar;
 
-@RequiresApi(api = Build.VERSION_CODES.O)
-public class NewToDoFragment extends DialogFragment implements TimePicker.OnClickListener {
-    private TimePicker timePicker;
-    private EditText editText;
-    private String message;
-    private LocalDate date;
-    private LocalTime time;
-    private LocalDateTime endDateTime;
+public class NewToDoFragment extends DialogFragment {
+    private LocalDate localDate = LocalDate.now();
+    private TextInputEditText dateText, timeText;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        date = LocalDate.now();
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater factory = LayoutInflater.from(getActivity());
-        View textEntryView = factory.inflate(R.layout.new_to_do_fragment, null);
-        builder.setView(textEntryView);
-        editText = textEntryView.findViewById(R.id.taskTextView);
-        timePicker = textEntryView.findViewById(R.id.timePicker);
-        timePicker.setIs24HourView(true);
-        CalendarView calendarView = textEntryView.findViewById(R.id.calendarView2);
-        calendarView.setMinDate(Calendar.getInstance().getTimeInMillis());
+        View view = factory.inflate(R.layout.new_to_do_fragment, null);
+        builder.setView(view);
+        TextInputEditText descriptionText = view.findViewById(R.id.newtodotextinput);
+        dateText = view.findViewById(R.id.newtododateinput);
+        timeText = view.findViewById(R.id.newtodotimeinput);
+        dateText.setOnClickListener(dateClickListener);
+        timeText.setOnClickListener(timeClickListener);
+        builder.setTitle(getString(R.string.new_todo_dialog_title))
+                .setPositiveButton(getString(R.string.new_todo_dialog_confirm), (dialog, id) -> {
+                    String message = descriptionText.getText().toString();
+                    LocalDate endDate = LocalDate.parse(dateText.getText().toString());
+                    LocalTime endTime = LocalTime.parse(timeText.getText().toString());
 
-        builder.setTitle("Nowe zadanie")
-                .setPositiveButton("Stwórz", new DialogInterface.OnClickListener() {
-                    @TargetApi(Build.VERSION_CODES.O)
-                    @RequiresApi(api = Build.VERSION_CODES.M)
-                    public void onClick(DialogInterface dialog, int id) {
-                        message = editText.getText().toString();
-                        time = LocalTime.of(timePicker.getHour(), timePicker.getMinute());
-                        endDateTime = LocalDateTime.of(date, time);
-                        if(endDateTime.isBefore(LocalDateTime.now()))
-                            Toast.makeText(getContext(), "Nie możesz ustawić czasu który już minął.", Toast.LENGTH_SHORT).show();
-                        else if (message.isEmpty())
-                            Toast.makeText(getContext(), "Nie podałeś żadnego zadania", Toast.LENGTH_LONG).show();
-                        else {
-                            ToDoEvent toDoEvent = new ToDoEvent();
-                            toDoEvent.setOpis(message);
-                            toDoEvent.setDatastart(LocalDateTime.now());
-                            toDoEvent.setDatakoniec(endDateTime);
-                            ViewModelProviders.of(requireActivity()).get(ToDoViewModel.class).insert(toDoEvent);
-                        }
+                    LocalDateTime endDateTime = LocalDateTime.of(endDate, endTime);
+                    if(endDateTime.isBefore(LocalDateTime.now()))
+                        Toast.makeText(getContext(), getString(R.string.new_todo_bad_time), Toast.LENGTH_SHORT).show();
+                    else if (message.isEmpty())
+                        Toast.makeText(getContext(), getString(R.string.new_todo_bad_text), Toast.LENGTH_LONG).show();
+                    else {
+                        ToDoEvent toDoEvent = new ToDoEvent();
+                        toDoEvent.setOpis(message);
+                        toDoEvent.setDatastart(LocalDateTime.now());
+                        toDoEvent.setDatakoniec(endDateTime);
+                        new ViewModelProvider(requireActivity()).get(ToDoViewModel.class).insert(toDoEvent);
                     }
                 })
-                .setNegativeButton("Anuluj", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                    }
+                .setNegativeButton(getString(R.string.cancel), (dialog, id) -> {
                 });
-
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @RequiresApi(api = Build.VERSION_CODES.O)
-            @Override
-            public void onSelectedDayChange(CalendarView calendarView, int rok, int mies, int dzien) {
-                mies++; //miesiace z 0-11 na 1-12
-                date = LocalDate.of(rok,mies,dzien);
-
-            }
-        });
         return builder.create();
     }
 
+    private View.OnClickListener dateClickListener = (v)->{
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), (datePicker, i, i1, i2) -> {
+            datePicker.setMinDate(Calendar.getInstance().getTimeInMillis());
+            dateText.setText(LocalDate.of(i, i1+1, i2).toString());
+        }, localDate.getYear(), localDate.getMonthValue()-1, localDate.getDayOfMonth());
 
-    @Override
-    public void onClick(View view) {
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+        datePickerDialog.show();
+    };
 
-    }
+    private View.OnClickListener timeClickListener = (v)->{
+        new TimePickerDialog(getContext(), (timePicker, i, i1) -> {
+            timeText.setText(LocalTime.of(i, i1).toString());
+        }, 12, 0, true).show();
+    };
+
 
 }
